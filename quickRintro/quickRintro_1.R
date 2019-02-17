6d086cbbb4deed9590471b082f85ea5e64dd0782
zdealveindy <zeleny.david@gmail.com>
2019-02-17 18:47:52 +0800

    Create quickRintro_1.R

---
+++ quickRintro/quickRintro_1.R
@@ -0,0 +1,8 @@
+# This is my first try of using R
+
+1+1
+sqrt (25)
+a <- sqrt (25)   # assign the result of squareroot calculation into var. a
+
+plot (dist ~ speed, data = cars)
+plot (dist ~ speed, data = cars, main = 'Dependence of distance on speed')  # this is a plotting function with main header
